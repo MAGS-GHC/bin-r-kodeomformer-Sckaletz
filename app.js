fca09840fc1d4198153205c5e6308a3c55bbe28c
Sckaletz <allan@leeloo.dk>
2023-11-09 21:02:55 +0100

    Final

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
 const binaryNumbers = [128, 64, 32, 16, 8, 4, 2, 1];
 
-// Function to calculate binary to IP
+// Funktion til at beregne IP ud fra binært tal
 function bintoip() {
-    let binaryToIP = 0;
-    let binaryToIPArray = [];
-    let binaryToIPValue = document.getElementById("binarytoip").value;
-    let validBinary = /^[01.]+$/.test(binaryToIPValue);
-    const binaryToIPValueSplit = binaryToIPValue.split(".");
+    let binaryToIP = 0; // IP værdi mens den kører gennem loopet, som gemmes som en int
+    let binaryToIPArray = []; // Samling af endelige IP dele efter de bliver beregnet ovenfor
+    let binaryToIPValue = document.getElementById("binarytoip").value; // Værdien fra input feltet
+    let validBinary = /^[01.]+$/.test(binaryToIPValue); // Input værdien tjekkes for lovlige tal
+    const binaryToIPValueSplit = binaryToIPValue.split("."); // Det binære tal deles op, hvor der er punktummer.
     if (validBinary) {
-        for (part in binaryToIPValueSplit) {
-            let binaryArrayPart = binaryToIPValueSplit[part];
-            for (index in binaryArrayPart) {
-                if (Number(binaryArrayPart[index]) === 1) {
-                    binaryToIP += binaryNumbers[index];
+        for (part in binaryToIPValueSplit) { // Hver del af den binære kode loopes nu igennem (eks. 11000100)
+            let binaryArrayPart = binaryToIPValueSplit[part]; 
+            for (index in binaryArrayPart) { // Hvert tal i delen loopes nu igennem (eks. 1)
+                if (Number(binaryArrayPart[index]) === 1) { // Hvis tallet er 1....
+                    binaryToIP += binaryNumbers[index]; // ....tillægges værdien for samme index i vores binaryNumbers (eks. 128 for første tal)
                 }
             }
-            binaryToIPArray.push(binaryToIP);
-            binaryToIP = 0;
+            binaryToIPArray.push(binaryToIP); // Her pushes den endelige værdi for den binære talgruppe til vores array
+            binaryToIP = 0; // Der resettes og næste talgruppe køres igennem
         }
-        alert(`The IP is: ${binaryToIPArray[0]}.${binaryToIPArray[1]}.${binaryToIPArray[2]}.${binaryToIPArray[3]}`);
+        alert(`The IP for the binary value:\n${binaryToIPValue}\nis:\n${binaryToIPArray[0]}.${binaryToIPArray[1]}.${binaryToIPArray[2]}.${binaryToIPArray[3]}`); // Alert med endelig IP
     } else {
-        alert("Use only numbers 0-1 and '.'");
+        alert("Use only numbers 0-1 and '.'"); // Fejl hvis det binære tal eks. indeholder 2, 3 osv.
     }
-    document.getElementById("binarytoip").value = "";
+    document.getElementById("binarytoip").value = ""; // Clear input feltet
 }
 
-// Function to calculate IP to binary
+// Funktion til at beregne binær talgruppe ud fra IP
 function iptobin() {
-    let ipToBinary = "";
-    let ipToBinaryArray = [];
-    let ipToBinaryValue = document.getElementById("iptobinary").value;
-    let validIP = /^[0123456789.]+$/.test(ipToBinaryValue);
-    const ipToBinaryValueSplit = ipToBinaryValue.split(".");
+    let ipToBinary = ""; // Binær talgruppe mens den kører gennem loopet, som gemmes som en string
+    let ipToBinaryArray = []; // Samling af endelige binære talgrupper efter de er blevet samlet ovenfor
+    let ipToBinaryValue = document.getElementById("iptobinary").value; // Værdien fra input feltet
+    let validIP = /^[0123456789.]+$/.test(ipToBinaryValue); // Input værdien tjekkes for lovlige tal
+    const ipToBinaryValueSplit = ipToBinaryValue.split("."); // IP deles op, hvor der er punktummer
     if (validIP) {
-        for (index in ipToBinaryValueSplit) {
-            let currentNumber = Number(ipToBinaryValueSplit[index]);
-            for (i in binaryNumbers) {
-                if (currentNumber >= binaryNumbers[i]) {
-                    currentNumber -= binaryNumbers[i];
-                    ipToBinary += "1";
+        for (part in ipToBinaryValueSplit) { // Hver del af IP'en loopes nu igennem (eks. 192)
+            let currentNumber = Number(ipToBinaryValueSplit[part]); // Tallet konverteres til en int
+            for (i in binaryNumbers) { 
+                if (currentNumber >= binaryNumbers[i]) { // Tallet tjekkes i henhold til binaryNumbers (eks. 128)
+                    currentNumber -= binaryNumbers[i]; // Tallet trækkes fra
+                    ipToBinary += "1"; // Hvis tallet var stort nok til at kunne fratrækkes tallet fra binaryNumbers tildeles en string 1
                 } else {
-                    ipToBinary += "0";
+                    ipToBinary += "0"; // Ellers tildeles string 0
                 }
             }
-            ipToBinaryArray.push(ipToBinary);
-            ipToBinary = "";
+            ipToBinaryArray.push(ipToBinary); // Her pushes den endelige binære værdi for IP tallet til vores array
+            ipToBinary = ""; // Der resettes og næste tal i IP'en køres igennem
         }
-        alert(`The Binary number is: ${ipToBinaryArray[0]}.${ipToBinaryArray[1]}.${ipToBinaryArray[2]}.${ipToBinaryArray[3]}`);
+        alert(`The Binary number for the IP:\n${ipToBinaryValue}\nis:\n${ipToBinaryArray[0]}.${ipToBinaryArray[1]}.${ipToBinaryArray[2]}.${ipToBinaryArray[3]}`); // Alert med endelige binære talgruppe
     } else {
-        alert("Use only numbers 0-9 and '.'");
+        alert("Use only numbers 0-9 and '.'"); // Fejl hvis IP'en indeholder eksempelvis bogstaver
     }
-    document.getElementById("iptobinary").value = "";
+    document.getElementById("iptobinary").value = ""; // Clear input feltet
 }
